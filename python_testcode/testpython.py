c95f9ec01384ed111b4a13ed0f63958efdaab32a
Devashish Rattan <devashish.rattan@magicsw.com>
2019-02-01 22:24:07 +0530

    Home machine code - WIN

---
+++ python_testcode/testpython.py
@@ -0,0 +1,11 @@
+print ("This is a program to get the sum of two numbers")
+#get numbers
+num1 = input('enter the first number')
+num2 = input('enter the second number')
+#numbers are stored in num1 and num2
+
+#logic
+sum = float(num1) + float(num2)
+
+#output
+print('the sum of {0} and {1} is {2}'.format(num1,num2,sum))
