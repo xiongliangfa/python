784c91e9bb3370412c69396749ba9802a870aec0
devashishrattan <devashishrattan@magicsw.com>
2019-02-16 21:25:42 +0530

    files committed

--- development/HMHAudioMerge.py
+++ development/HMHAudioMerge.py
@@ -2,8 +2,14 @@ import speech_recognition as sr
 import subprocess
 import wave,os
 import pydub
+from tkinter import *
+from tkinter import messagebox
+
 # global deceleration
 file_list = []
+win = Tk()
+win.title('Audio Merge tool V2.1 - HMH')
+mystring = StringVar()
 
 #func to merge the audio
 def mergeAudio(aud1,aud2,outputAudio):
@@ -43,30 +49,38 @@ def addToArray(filePath):
         file_list.append(file)
 
 # # adding silence
-# def addSilence(aud1):
-#     infiles = [aud1, 'silence.wav']
-#     outfile = aud1
-#     data= []
-#     for infile in infiles:
-#         w = wave.open(infile, 'rb')
-#         data.append( [w.getparams(), w.readframes(w.getnframes())] )
-#         w.close()
-#     output = wave.open(outfile, 'wb')
-#     output.setparams(data[0][0])
-#     output.writeframes(data[0][1])
-#     output.writeframes(data[1][1])
-#     output.close()
+def addSilence(aud1):
+    infiles = [aud1, 'silence\\silence.wav']
+    outfile = aud1
+    data= []
+    for infile in infiles:
+        w = wave.open(infile, 'rb')
+        data.append( [w.getparams(), w.readframes(w.getnframes())] )
+        w.close()
+    output = wave.open(outfile, 'wb')
+    output.setparams(data[0][0])
+    output.writeframes(data[0][1])
+    output.writeframes(data[1][1])
+    output.close()
 
-if __name__ == "__main__":
-    print('Enter the directory')
-    filePath = input(">")   
+def trigger():
+    filePath = mystring.get()  
     addToArray(filePath)
     # toWav(filePath)
     file = open('log.txt','w')
     for i in range(0,len(file_list),2):
-        outputAudio = SpeechToText(file_list[i],i)
-        # addSilence(file_list[i])
-        mergeAudio(file_list[i],file_list[i+1],outputAudio)
-        print(outputAudio,' Created')
-        file.write(file_list[i] + ' + ' + file_list[i+1] +' --> ' + outputAudio + '\n')
+        if not file_list[i] == 'silence':
+            outputAudio = SpeechToText(file_list[i],i)
+            addSilence(file_list[i])
+            mergeAudio(file_list[i],file_list[i+1],outputAudio)
+            print(outputAudio,' Created')
+            file.write(file_list[i] + ' + ' + file_list[i+1] +' --> ' + outputAudio + '\n')
     file.close()
+
+if __name__ == "__main__":
+    Label(win, text="Enter Audio Path").pack()  #label
+    Entry(win, textvariable = mystring,width=50).pack() #textblock
+    button = Button(win, text="Proceed", command=trigger) #button
+    button.pack()
+    win.geometry("600x300+500+250")
+    win.mainloop()
